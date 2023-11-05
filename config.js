2596415a09ee748179113fe91cd45e7f96571fba
baagii <baaggii234@gmail.com>
2023-11-05 21:32:30 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Only for 요정.",
+    introTitle: '🖤',
+    introDesc: `요정`,
+    btnIntro: '^^HiHi^^',
+    title: 'hrhn_sojo🥰',
+    desc: 'only ',
+    btnYes: 'Үргэлжлүүлэх<33',
+    btnNo: 'Устгах:3',
+    question: 'Нэрээ оруулна уу<3',
+    btnReply: '🖤<3',
+    reply: 'sojoo<3',
+    mess: 'Баярлалаа😘😘',
+    messDesc: '......',
+    btnAccept: 'Next<3',
+    messLink: 'https://shisui1117.github.io/arth_ur/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
